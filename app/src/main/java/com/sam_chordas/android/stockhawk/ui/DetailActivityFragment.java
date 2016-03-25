61516019ecb53c9f0792371e7e1656c9363abf03
Christian Loosli <loosli.christian@gmail.com>
2016-03-25 16:32:49 +0100

    load quote and display the symbol

--- app/src/main/java/com/sam_chordas/android/stockhawk/ui/DetailActivityFragment.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/DetailActivityFragment.java
@@ -1,11 +1,13 @@
 package com.sam_chordas.android.stockhawk.ui;
 
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,7 +16,6 @@ import android.widget.TextView;
 import com.sam_chordas.android.stockhawk.R;
 import com.sam_chordas.android.stockhawk.data.QuoteColumns;
 import com.sam_chordas.android.stockhawk.data.QuoteDatabase;
-import com.sam_chordas.android.stockhawk.data.QuoteProvider;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -25,16 +26,18 @@ import butterknife.ButterKnife;
 public class DetailActivityFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
     static final String DETAIL_URI = "URI";
+    static final int COL_QUOTE_ID = 0;
+    static final int COL_QUOTE_SYMBOL = 1;
+    private static final String LOG_TAG = DetailActivityFragment.class.getSimpleName();
     private static final int CURSOR_LOADER_ID = 0;
-    private Cursor mCursor;
-
     private static final String[] DETAIL_COLUMNS = {
             QuoteDatabase.QUOTES + "." + QuoteColumns._ID,
-    };
-    static final int COL_QUOTE_ID = 0;
+            QuoteColumns.SYMBOL,
 
+    };
     @Bind(R.id.textview)
     TextView mTextView;
+    private Uri mUri;
 
     public DetailActivityFragment() {
     }
@@ -43,10 +46,10 @@ public class DetailActivityFragment extends Fragment implements LoaderManager.Lo
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-//        Bundle arguments = getArguments();
-//        if (arguments != null) {
-//            mUri = arguments.getParcelable(DetailFragment.DETAIL_URI);
-//        }
+        Bundle arguments = getArguments();
+        if (arguments != null) {
+            mUri = arguments.getParcelable(DetailActivityFragment.DETAIL_URI);
+        }
         final View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
 
         ButterKnife.bind(this, rootView);
@@ -65,22 +68,37 @@ public class DetailActivityFragment extends Fragment implements LoaderManager.Lo
     }
 
     @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        getLoaderManager().initLoader(CURSOR_LOADER_ID, null, this);
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        return new CursorLoader(
-                getActivity(),
-                QuoteProvider.Quotes.withSymbol(""),
-                DETAIL_COLUMNS,
-                null,
-                null,
-                null
-        );
+        Log.v(LOG_TAG, "In onCreateLoader");
+        if (null != mUri) {
+            // Now create and return a CursorLoader that will take care of
+            // creating a Cursor for the data being displayed.
+            return new CursorLoader(
+                    getActivity(),
+                    mUri,
+                    DETAIL_COLUMNS,
+                    null,
+                    null,
+                    null
+            );
+        }
+        return null;
     }
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-        mCursor = data;
-//        findRange(mCursor);
-//        fillLineSet();
+        if (data != null && data.moveToFirst()) {
+            String symbol = data.getString(COL_QUOTE_SYMBOL);
+            mTextView.setText(symbol);
+            //        findRange(mCursor);
+            //        fillLineSet();
+        }
     }
 
     @Override
